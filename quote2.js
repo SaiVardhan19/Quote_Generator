e178319ab6a352ba65d7b8dcc871134d05897b68
Sai Vardhan <128779566+SaiVardhan19@users.noreply.github.com>
2024-08-26 12:35:16 +0530

    Update quote2.js

--- quote2.js
+++ quote2.js
@@ -1,4 +1,4 @@
-
+// The variable quotes is an array of dictionaries that contains "quotes" and "author" as the keys 
 const quotes = [
     { quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.", author: "Nelson Mandela" },
     { quote: "The way to get started is to quit talking and begin doing.", author: "Walt Disney" },
@@ -8,11 +8,13 @@ const quotes = [
     { quote: "The future belongs to those who believe in the beauty of their dreams.", author: "Eleanor Roosevelt" }
 ];
 
+// This function is used to generate a new quote by generating returning a dictionary in the quotes array at random position with the help of the Math Library 
 function generateRandomQuote() {
     const randomNumber = Math.floor(Math.random() * quotes.length);
     return quotes[randomNumber];
 }
 
+// This function is used to generate a new quote by invoking the generateRandomQuote() function and printing the quote and the author 
 function updateQuote() {
     const quoteElement = document.getElementById('quote');
     const authorElement = document.getElementById('author');
@@ -21,7 +23,9 @@ function updateQuote() {
     authorElement.textContent = "- " + randomQuote.author;
 }
 
+//This statement is invoked when the "New Quote" button is used through the index.html file
 const newQuoteButton = document.getElementById('new-quote');
 newQuoteButton.addEventListener('click', updateQuote);
 
+// This statement is the driver code for the entire file that invokes the updateQuote() 
 updateQuote();
